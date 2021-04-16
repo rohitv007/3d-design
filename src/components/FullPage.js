c16b68b2a812e3694c4284d60fee69e8d4c8022a
ROHIT VIJAYVARGIYA <66072894+rohitv007@users.noreply.github.com>
2021-04-17 02:43:38 +0530

    updated fullpage.js

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -2,58 +2,75 @@ import React from 'react'
 import Home from './Home';
 import About from './About';
 import Testimonials from './Testimonials';
-import ReactFullpage from '@fullpage/react-fullpage';
+// import ReactFullpage from '@fullpage/react-fullpage';
 import '../css/fullp.css'
 import '../js/scrolling'
+import ScrollableContainer from "react-full-page-scroll";
 
 
-const FullPage = () => (
 
-    <ReactFullpage 
-        licenseKey = {null}                       // required
-        navigation =  {true}
-        navigationTooltips = {['Home', 'About', 'Testimonials']}
-        autoScrolling = {true}    
-        scrollingSpeed = {1000}
-        controlArrows = {false}
-        slidesNavigation = {true}
-        scrollHorizontally = {true}
-        fitToSection = {true}
-        delay = {0}
-        // anchors = {['s1', 's2', 's3']}
+// const FullPage = () => (
 
-        render={() => {
+//     <ReactFullpage 
+//         licenseKey = {null}                       // required
+//         navigation =  {true}
+//         navigationTooltips = {['Home', 'About', 'Testimonials']}
+//         autoScrolling = {true}    
+//         scrollingSpeed = {1000}
+//         controlArrows = {false}
+//         slidesNavigation = {true}
+//         scrollHorizontally = {true}
+//         fitToSection = {true}
+//         delay = {0}
+//         // anchors = {['s1', 's2', 's3']}
 
-            return (
-                <div id='3d_app'>
-                    <ReactFullpage.Wrapper>
-                        <div className="section section1">
-                            <div className='home_comp'>
-                                <Home/>
-                            </div>
-                        </div>
-                        <div  className="section section2">
-                            <div className='slide'>
-                                <About/>
-                            </div>
-                            <div className="slide">
-                                <About/>
-                            </div>
-                            <div className="slide">
-                                <About/>
-                            </div>
-                        </div>
-                        <div className="section section4">
-                            <div className='test_comp'>
-                                <Testimonials/>
-                            </div>
-                        </div>
-                    </ReactFullpage.Wrapper>
-                </div>
-            );
-        }}
-    />
-)
+//         render={() => {
+
+//             return (
+//                 <div id='3d_app'>
+//                     <ReactFullpage.Wrapper>
+//                         <div className="section section1">
+//                             <div className='home_comp'>
+//                                 <Home/>
+//                             </div>
+//                         </div>
+//                         <div  className="section section2">
+//                             <div className='slide'>
+//                                 <About/>
+//                             </div>
+//                             <div className="slide">
+//                                 <About/>
+//                             </div>
+//                             <div className="slide">
+//                                 <About/>
+//                             </div>
+//                         </div>
+//                         <div className="section section4">
+//                             <div className='test_comp'>
+//                                 <Testimonials/>
+//                             </div>
+//                         </div>
+//                     </ReactFullpage.Wrapper>
+//                 </div>
+//             );
+//         }}
+//     />
+// )
+
+function PageComponent({children}){
+    return (<div>{children}</div>)
+}
+   
+   
+function FullPage() {
+    return (
+        <ScrollableContainer animationTime={500}>
+            <PageComponent><Home/></PageComponent>
+            <PageComponent><About/></PageComponent>
+            <PageComponent><Testimonials/></PageComponent>
+        </ScrollableContainer>
+    );
+}   
 
 export default FullPage
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,16 +7,7 @@ function Home() {
         <div className='home'>
             {/* <div className="container"> */}
             <div className='home_video'>   
-                {/* <iframe 
-                    width="100%" height="100%" 
-                    src="https://www.youtube.com/embed/4fQeaM62mOY" 
-                    title="YouTube video player" 
-                    frameBorder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowFullScreen
-                    scrolling="yes"
-                /> */}
-                <video data-keepplaying data-autoplay muted width='100%' loop>
+                <video keepPlaying autoPlay muted width='100%' loop>
                     <source src={homevid} type="video/mp4"/>
                 </video>
             </div>
