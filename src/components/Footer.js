641631dc89f77f9f6cac847023bd1f3f0eece4d2
ROHIT VIJAYVARGIYA <66072894+rohitv007@users.noreply.github.com>
2021-04-21 22:44:33 +0530

    icon styles...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,10 +15,10 @@ function Footer() {
                 </div>
                 <div className="footer_right col-12 col-sm-4">
                     <ul className='social_links'>
-                        <li><a target='blank' rel='noopener noreferrer' href='https://www.instagram.com/'><FiInstagram color='red' size="1.2em"/></a></li>
-                        <li><a target='blank' rel='noopener noreferrer' href='https://twitter.com/'><FiTwitter color='#00aced' size="1.2em"/></a></li>
-                        <li><a target='blank' rel='noopener noreferrer' href='tel: +4733378901'><FiPhone color='green' size="1.2em"/></a></li>
-                        <li><a target='blank' rel='noopener noreferrer' href="mailto:someone@example.com"><FiMail color='darkcyan' size="1.2em"/></a></li>
+                        <li><a target='blank' rel='noopener noreferrer' href='https://www.instagram.com/'><FiInstagram size="1.2em"/></a></li>
+                        <li><a target='blank' rel='noopener noreferrer' href='https://twitter.com/'><FiTwitter size="1.2em"/></a></li>
+                        <li><a target='blank' rel='noopener noreferrer' href='tel: +4733378901'><FiPhone size="1.2em"/></a></li>
+                        <li><a target='blank' rel='noopener noreferrer' href="mailto:someone@example.com"><FiMail size="1.2em"/></a></li>
                     </ul>
                 </div>
             </div>
