de5eb5cf3fee1833c9a255033dd6c5921439af37
ROHIT VIJAYVARGIYA <66072894+rohitv007@users.noreply.github.com>
2021-04-26 21:32:52 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
       <FullPage/>
       <ContactModal open={showModal} close={() => setShowModal(false)}/>
       <StickyIcons/>
-      <a href='https://www.flaticon.com/packs/social-media-logos-2' className="sr-only">FlatIcon</a>
     </div>
   );
 }
